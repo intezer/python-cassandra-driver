b4220f0e7d5c950ec5116f5a2e2d60157e56550c
davidt99 <davidtufik@gmail.com>
2017-05-16 23:41:12 +0300

    fix(script): fixed the script

--- create-images.sh
+++ create-images.sh
@@ -5,25 +5,28 @@ python_versions=();
 IFS=$'\n'
 driver_versions=(`cat $1`)
 python_versions=(`cat $2`)
+IFS=$' \t\n'
 
 for driver_version in "${driver_versions[@]}"
 do
     for python_version_aliases in "${python_versions[@]}"
     do
         python_version_aliases=(${python_version_aliases})
-        tags_command_string=()
+        tags_args=()
         for python_version in "${python_version_aliases[@]}"
         do
-            tags_command_string+=(-t "intezer/python-cassandra-driver:$python_version-$driver_version")
+            if [[ ${python_version} != latest ]]; then
+                tags_args+=(-t "intezer/python-cassandra-driver:$python_version-$driver_version")
+            fi
             if [[ ${driver_versions[0]} == ${driver_version} ]]; then
-                tags_command_string+=(-t "intezer/python-cassandra-driver:$python_version")
+                tags_args+=(-t "intezer/python-cassandra-driver:$python_version")
             fi
         done
-        docker build --pull --build-arg python_version=${python_version_aliases[0]} --build-arg driver_version="$driver_version" "${tags_command_string[@]}" .
-        for i in "${!tags_command_string[@]}"
+        docker build --pull --build-arg python_version="${python_version}" --build-arg driver_version="$driver_version" "${tags_args[@]}" .
+        for i in "${!tags_args[@]}"
         do
-            if [[ $i%2==0 ]]; then
-                docker push ${tags_command_string[i]}
+            if [[ $((i%2)) -eq 1 ]]; then
+                docker push ${tags_args[i]}
             fi
         done
     done
